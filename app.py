2fd422815b29c98a738ac0ca23e00b19819de5e9
SouvikUMD <87155394+SouvikUMD@users.noreply.github.com>
2024-06-26 17:27:17 -0400

    Changed to "html_on_flask" app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 25 15:56:03 2024
+Created on Fri Dec 22 13:05:54 2023
 
 @author: prama
 """
 
-
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       first_name = request.form.get("fname")
+       # getting input with name = lname in HTML form
+       last_name = request.form.get("lname")
+       return "Your name is "+first_name + last_name
+    return render_template("index.html")
+
+if __name__ == '__main__':
     app.run()
