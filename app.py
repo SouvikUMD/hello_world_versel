b16d7216825ed5d4b22208e3bb1b23d79d1024a8
SouvikUMD <87155394+SouvikUMD@users.noreply.github.com>
2024-06-26 21:30:11 -0400

    Changed "vercel.json" and "app.py" back.

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec 22 13:05:54 2023
+Created on Tue Jun 25 15:56:03 2024
 
 @author: prama
 """
 
-from flask import Flask, request, render_template
+
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
-# A decorator used to tell the application
-# which URL is associated function
-@app.route('/', methods =["GET", "POST"])
-def gfg():
-    if request.method == "POST":
-       # getting input with name = fname in HTML form
-       first_name = request.form.get("fname")
-       # getting input with name = lname in HTML form
-       last_name = request.form.get("lname")
-       return "Your name is "+first_name + last_name
+@app.route("/")
+def hello():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
